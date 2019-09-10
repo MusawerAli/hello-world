4389fa151b15cde0b54b75eb6a1cba5fc4026ffa
MusawerAli <pakjalihouse@gmail.com>
2019-09-10 10:04:39 +0500

    clone

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,163 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-test',
+  template: `
+
+          <div>Test URl</div>
+          <p>{{2+3}}</p>
+          <p>Welcome {{name}}</p>
+          <p>{{greet('Hamza')}}</p>
+          <p>{{alert}}</p>
+          <p>{{name.toUpperCase()}}</p>
+          <p>{{name.length}}</p>
+          <input value="Norway"> 
+          <input [id]="myid" value="BNBN">
+          <input [disabled] = "isDisabled" id="{{myid}}" value="BNBN">
+          <p class="text-success">class binding</p>
+          <p [class]="successClass">class binding</p>
+          <p [class.text-special]="hasError">class binding</p>
+          <p [ngClass]="messageClasses">Class Bind</p>
+          <h2 style="color:teal">Style Binding</h2>
+          <h2 [style.color]="highlightColor">Style Binding</h2>
+          <h2 [style.color]="hasError ? 'red' :highlightColor ">Style Binding</h2>
+          <h2 [style.color]="hasError ? 'red' :highlightColor ">Style Binding</h2>
+          <h2 [style.color] = "'orange'">Style Binding</h2>
+          <h2 [ngStyle] = "styleClasses">Style Binding</h2>
+          <hr>
+          <h2>Event Binding</h2>
+          <button (click)="onclick()">Event Bind</button>
+          <button (click)="eventClick($event)">Event Bind </button>
+          <p [ngClass]="messageClasses">{{eventMessage}}</p>
+          
+          <hr>
+
+          <input  #myinput type="text">
+          <button (click)="myInput(myinput.value)">Click</button>
+          <hr>
+
+          <h2>Two way data bindingh</h2>
+          <input type="text" [(ngModel)]="name">
+          {{name}}
+
+          <hr>
+          <h2>Structural Directive</h2>
+          <h2 *ngIf = "true">NGIF true</h2>
+          <h2 *ngIf = "false">NGIF true</h2>
+          <h2 *ngIf="displayName">NameThis is declear as true<h2>
+
+            <p>How to render if else this</p>
+
+            <h2 *ngIf="condition;else elseblock">IF else block</h2>
+            <ng-template #elseblock>
+            <h2 [ngClass]="messageClasses">
+            Else Block
+            </h2>
+            </ng-template>
+
+          <div *ngIf="displayName; then thenBlock;else elseBlock">
+          
+          </div>
+
+            <ng-template #thenBlock>
+              <h3>
+                  this is true condition
+              </h3>
+            </ng-template>
+
+            <ng-template #elseBlock>
+            <h3>
+                this is false condition
+            </h3>
+            </ng-template>
+
+            <hr>
+            <h3>ngSwitch</h3>
+            <div [ngSwitch]="color">
+                <div *ngSwitchCase="'red'">YOu choose red color</div>
+                <div [style.color]="color" *ngSwitchCase="'blue'" >YOu choose blue color</div>
+                <div *ngSwitchCase="'yellow'">YOu choose yellow color</div>
+                <div *ngSwitchDefault>Plesase Put value in color</div>
+            </div>
+            <hr>
+            <h3>ngFor</h3>
+
+            <div *ngFor="let car of cars;index as i">
+            <b>{{i}}{{car}}</b>
+            </div>
+
+            <div *ngFor="let car of cars;first as f">
+            <b>{{f}}{{car}}</b>
+            </div>
+
+          `,
+          styles: [`
+  div {
+    color:red;
+  }
+
+  .text-success {
+    color:green;
+  }
+  .text-danger {
+    color:red;
+  }
+
+  .text-special {
+    font-style:italic;
+  }
+  `]
+})
+export class TestComponent implements OnInit {
+
+  public name = "ALI";
+  public myid = "testId";
+  public isDisabled = true;
+  public successClass = "text-success";
+  public hasError = true;
+  public hasSpecial = true;
+  public highlightColor = "teal";
+  public eventMessage = "";
+  public displayName = true;
+  public condition = false;
+  public color = "blue";
+
+  public cars = ["BMW","MERC","FORD","Bently"];
+  public messageClasses = {
+    "text-success":!this.hasError,
+    "text-danger":this.hasError,
+    "text-special":this.hasSpecial
+  }
+
+  public styleClasses = {
+    color: "red",
+    fontStyle: "italic",
+    fontFamily: "Arial"
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onclick(){
+    console.log('Event bind');
+  }
+  greet(name){
+    return  "Hello " + this.name + " And " + name ;
+  }
+
+  eventClick(event){
+
+    this.eventMessage = "YOu" + event.type + "Event Bind button" ;
+    console.log(event);
+  }
+
+ myInput(value){
+  console.log(value);
+ }
+  public alert = alert('Hello');
+
+}
